c7c49459d8e401be812d3c7ee285bf6e2194727d
Milan Zagorac <zagoracm@gmail.com>
2023-01-21 12:49:41 -0500

    some files, remove MUI

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -4,16 +4,27 @@ import Button from "./Button";
 
 const MainContainer: React.FC = (): JSX.Element => {
   const [length, setLength] = useState<number>(12);
+  const [password, setPassword] = useState<string>("");
   const [hasLowerCase, setHasLowerCase] = useState<boolean>(false);
   const [hasUpperCase, setHasUpperCase] = useState<boolean>(false);
+  const [hasNumbers, setHasNumbers] = useState<boolean>(false);
+  const [hasSymbols, setHasSymbols] = useState<boolean>(false);
 
   const handleGenerationClick = () => {
-    //
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = '`!@#$%^&*()_+{}[]:"\\<>?/|';
+
+    let combinations = [];
+    if (hasLowerCase) combinations.push(lower);
+    if (hasUpperCase) combinations.push(upper);
+    if (hasNumbers) combinations.push(numbers);
+    if (hasSymbols) combinations.push(symbols);
   };
 
   return (
-    <div>
-      MainContainer
+    <div className="main-container">
       <Button text="test" onClick={handleGenerationClick} />
     </div>
   );
